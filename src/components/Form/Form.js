84d6965e20faa5a2420814057b16a3aeabcb0616
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-05-23 16:26:19 +0200

    "edit form.css"

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,16 +3,18 @@ export default function Form() {
   return (
     <>
       (//Form section)
-      <form id="form">
-        <h3>NEW ENTRY</h3>
-        <label for="motto"> Motto</label>
-        <input type="text" id="motto" name="motto"></input>
-        <label for="notes">Notes </label>
-        <textarea id="notes" name="notes"></textarea>
-        <button type="submit" value="submit">
-          Create
-        </button>
-      </form>
+      <div>
+        <form id="form">
+          <h3>NEW ENTRY</h3>
+          <label for="motto"> Motto</label>
+          <input type="text" id="motto" name="motto"></input>
+          <label for="notes">Notes </label>
+          <textarea id="notes" name="notes"></textarea>
+          <button type="submit" value="submit">
+            Create
+          </button>
+        </form>
+      </div>
     </>
   );
 }
