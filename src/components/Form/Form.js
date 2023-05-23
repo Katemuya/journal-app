80eed12590c181cfa07c70b37410f84d34c53e30
Catherine Muya <129610930+Katemuya@users.noreply.github.com>
2023-05-23 23:19:21 +0200

    "edit Form .css and .js"

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,19 @@
 import "./Form.css";
-export default function Form() {
+export default function Form({ onAddEntry }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    onAddEntry(data);
+    event.target.reset();
+    event.target.elements.motto.focus();
+  }
   return (
     <>
-      (//Form section)
       <div>
-        <form id="form">
+        <form id="form" onSubmit={handleSubmit}>
           <h3>NEW ENTRY</h3>
           <label for="motto"> Motto</label>
           <input type="text" id="motto" name="motto"></input>
